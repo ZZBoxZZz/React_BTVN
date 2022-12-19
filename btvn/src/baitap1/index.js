7a537473d30ebc265d52b382c5774741f193364d
ZZBoxZZz <huydang1609.dpmh@gmail.com>
2022-12-19 22:24:37 +0700

    second update

---
+++ btvn/src/baitap1/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import Head from "./head";
+import Body from "./body";
+import Item from "./item";
+import Footer from "./footer";
+export default class BaiTap1 extends Component {
+  render() {
+    return (
+      <>
+        <Head />
+        
+        <Body />
+
+        <Item />
+
+        <Footer />
+          
+        
+      </>
+    );
+  }
+}
